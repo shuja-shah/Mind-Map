ffa242aa7016a2932fd405a936dfb48c475e2c43
shuja-shah <shujashah786@gmail.com>
2023-06-06 21:20:13 +0500

    Finalized Infra Structure of the main app

--- src/InnerApp.js
+++ src/InnerApp.js
@@ -3,7 +3,14 @@ import Tree from "react-d3-tree";
 
 const SideNav = () => {
   return (
-    <Grid item container direction="column" sx={{ height: "100%" }}>
+    <Grid
+      item
+      container
+      direction="column"
+      sx={{
+        height: "100%",
+      }}
+    >
       <Grid item>Past Project 1</Grid>
       <Grid item>Past Project 2</Grid>
       <Grid item>Past Project 3</Grid>
@@ -57,11 +64,24 @@ const MainNode = () => {
 const InnerApp = () => {
   return (
     <Grid container alignItems={"flex-start"}>
-      <Grid item xl={3} lg={3} md={3} sm={3} xs={3} sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xl={1.5}
+        lg={1.5}
+        md={1.6}
+        sm={1.5}
+        xs={1}
+        sx={{
+          height: "100vh",
+          borderRadius: "8px",
+          border: "1px solid #d9d9d9",
+          background: "#f1f3f5",
+        }}
+      >
         <SideNav />
       </Grid>
 
-      <Grid item>
+      <Grid item xl={10} lg={10} md={10} sx={{ height: "100vh" }}>
         <MainNode />
       </Grid>
     </Grid>
