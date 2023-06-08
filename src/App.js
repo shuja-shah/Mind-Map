a7e8eb63e840c7b4b9b19cb938bd20da6982cfdb
Syed Shuja Hussain (He/Him) <shujashah786@gmail.com>
2023-06-09 00:54:13 +0500

    Updare main Route

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
       <Routes>
         <Route exact path="/Auth" element={<Auth />} />
         <Route element={<RequireAuth />}>
-          <Route exact path="/" element={<InnerApp />} />
-          <Route exact path="/List" element={<Layout />} />
+          <Route exact path="/check" element={<InnerApp />} />
+          <Route exact path="/" element={<Layout />} />
         </Route>
       </Routes>
     </Router>
